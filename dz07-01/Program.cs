2c1018e4947314ddef2fefde084ab100b3f7312d
Pavel Starodubov <pablostar68@yahoo.com>
2022-11-12 17:17:01 +0300

    add dz07-01

--- dz07-01/Program.cs
+++ dz07-01/Program.cs
@@ -1,2 +1,45 @@
 ﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
 
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Sortirovka(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < arr.GetLength(1); k++)
+            {
+                if (arr[i, j] < arr[i, k])
+                {
+                    int temp = arr[i, j];
+                    arr[i, j] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = new int[,]
+{
+    {4,5,3,9,4,11,20,78},
+    {7,2,9,1,8,25,14,95},
+};
+
+PrintArray(matrix);
+Console.WriteLine();
+
+Sortirovka(matrix);
+PrintArray(matrix);
